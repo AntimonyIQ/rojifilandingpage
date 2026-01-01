dbbc38434eb36813cfdc66063e170ad090a51ab7
AntimonyIQ <antimonyiq@gmail.com>
2026-01-01 11:33:51 -0800

    Refactor LogoutGuard component to improve readability and maintainability; streamline modal logic and ensure hash management during logout confirmation.

--- src/app/RedirectIfAuthenticated.tsx
+++ src/app/RedirectIfAuthenticated.tsx
@@ -2,23 +2,23 @@ import { session, SessionData } from "@/v1/session/session";
 import { Redirect, useRoute } from "wouter";
 
 export function RedirectIfAuthenticated({
-  path,
-  children,
+    path,
+    children,
 }: {
-  path: string;
-  children: React.ReactNode;
+        path: string;
+        children: React.ReactNode;
 }) {
-  const sd: SessionData = session.getUserData();
-  const [match] = useRoute(path);
+    const storage: SessionData = session.getUserData();
+    const [match] = useRoute(path);
 
-  if (!match) return null;
+    if (!match) return null;
 
-  // if (sd && sd.isLoggedIn) {
-  //     return <Redirect to="/dashboard/NGN" />;
-  // }
-  if (sd && Object.keys(sd.user).length) {
-    return <Redirect to="/dashboard/USD" />;
-  }
+    // if (storage && storage.isLoggedIn) {
+    //     return <Redirect to="/dashboard/NGN" />;
+    // }
+    if (storage && Object.keys(storage.user).length) {
+        return <Redirect to="/dashboard/USD" />;
+    }
 
-  return <>{children}</>;
+    return <>{children}</>;
 }
